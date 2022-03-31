4af99e27cf868a9292ca36d8b53c5a5a939d5f04
username git <email git>
2022-03-31 20:51:37 +0700

    Frontend_Pekan 6

---
+++ movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,50 @@
+import styles from "./AddMovieForm.module.css";
+
+// Menangkap props
+function AddMovieForm() {
+  return (
+    <div className={styles.container}>
+      <section className={styles.form}>
+        <div className={styles.form__left}>
+          <img
+            className={styles.form__image}
+            src="https://picsum.photos/536/354"
+            alt=""
+          />
+        </div>
+        <div className={styles.form__right}>
+          <h2 className={styles.form__title}>Add Movie Form</h2>
+          <form>
+            <div className={styles.form__group}>
+              <label htmlFor="title" className={styles.form__label}>
+                Title
+              </label>
+              <input
+                id="title"
+                className={styles.form__input}
+                type="text"
+                name="title"
+              />
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor="date" className={styles.form__label}>
+                Date
+              </label>
+              <input
+                id="date"
+                className={styles.form__input}
+                type="text"
+                name="date"
+              />
+            </div>
+            <div>
+              <button className={styles.form__button}>Add Movie</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AddMovieForm;
